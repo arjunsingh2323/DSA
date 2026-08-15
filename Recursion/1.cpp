4e1582e5dfe550577b7fe36da13c0ebe64a3bad0
Arjun Singh <arjunsingh232338@gmail.com>
2026-08-15 17:36:22 +0530

    Add DSA solutions - hashing, recursion, sorting

---
+++ Recursion/1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void name(int i,int n){
+    if(i>n){
+        return;
+    }
+    cout << "Arjun" << " ";
+    name(i+1,n);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    name(1,n);
+}
---
+++ Recursion/3.cpp
@@ -0,0 +1,17 @@
+//n to 1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int i, int n){
+    if(i<1){
+        return;
+    }
+    cout << i << endl;
+    f(i-1,n);
+} 
+int main (){
+    int n;
+    cin >> n;
+    f(n,n);
+}
