42e69f8131f5ea7a50f6c7d389cfacbb23e7653f
Arjun Singh <arjunsingh232338@gmail.com>
2026-09-01 23:17:03 +0530

    longestSubarrayWithSumK optimal added

---
+++ ARRAY/longestSubarrayWithSumK2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+including namespace std;
+
+int longestSubarrayWithSumK(vector<int> a;
+long long k){
+    int left = 0, right = 0;
+    long long sum = a[0];
+    int maxLen = 0;
+    int n = a.size();
+    while(right < n){
+        while(left <= right && sum > k){
+            sum -= a[left];
+            left++;
+        }
+        if(sum == k){
+            maxLen = max(maxLen,right-lrft+1);
+        }
+        right++;
+        if(right < n) sum += a[right];
+
+    }
+    return maxLen;
+    
+}
