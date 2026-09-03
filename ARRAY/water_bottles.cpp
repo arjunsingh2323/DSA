c3961febe1466c3c57948a71fddae462b41333e7
Arjun Singh <arjunsingh232338@gmail.com>
2026-09-04 00:18:37 +0530

    Water bottle problem added

---
+++ ARRAY/water_bottles.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int total = numBottles;
+        while(numBottles >= numExchange ){
+            int rem = numBottles % numExchange;
+            numBottles /= numExchange;
+            total += numBottles;
+            numBottles += rem; 
+        }
+        return total;
+        
+    }
+};
